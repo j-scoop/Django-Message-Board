49f74d75539ef9da5d4a8a0c094ccd7324f47595
Jack <j-bass@outlook.com>
2020-11-14 17:55:52 +0000

    Built db, implemented accessing of board objects in views.py templates

--- testProject/boards/views.py
+++ testProject/boards/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from .models import Board
 
 def home(request):
-    return HttpResponse('Hello, World')
+    boards = Board.objects.all()
+    boards_names = list()
 
+    for board in boards:
+        boards_names.append(board.name)
+
+    response_html = '<br>'.join(boards_names)
+
+    return HttpResponse(response_html)
