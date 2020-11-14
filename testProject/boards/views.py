613ba5383dace3c87eded3bfc392ec48dfa687fd
Jack <j-bass@outlook.com>
2020-11-14 18:23:35 +0000

    Created homepage template containing table of all boards in database

--- testProject/boards/views.py
+++ testProject/boards/views.py
@@ -1,13 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Board
 
 def home(request):
     boards = Board.objects.all()
-    boards_names = list()
 
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '<br>'.join(boards_names)
-
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
