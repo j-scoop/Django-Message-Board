54d4e2d324e55ee03b81750e98cd9cb035c11ec1
Jack <j-bass@outlook.com>
2020-11-15 14:18:52 +0000

    Added first test & added all untracked files

---
+++ testProject/boards/tests.py
@@ -0,0 +1,8 @@
+from django.urls import reverse
+from django.test import TestCase
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
