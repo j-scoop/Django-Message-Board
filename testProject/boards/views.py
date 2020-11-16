d4664b14ee2c4cc9b7a5e92b3af19aeab4579a2c
Jack <j-bass@outlook.com>
2020-11-16 19:57:24 +0000

    Added test to check for link to topic pages from homepage

--- testProject/boards/views.py
+++ testProject/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Board
 
 
@@ -9,5 +9,5 @@ def home(request):
 
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
+    board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
