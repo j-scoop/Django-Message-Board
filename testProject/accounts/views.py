937cb76c37b8f831bb0f9bc659a4cae888c6fdf3
Jack <j-bass@outlook.com>
2020-11-21 17:33:27 +0000

    Added tests for user creation

--- testProject/accounts/views.py
+++ testProject/accounts/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
