622134b6391bc11389d0ca184a899ce5c1ba19db
Jack <j-bass@outlook.com>
2020-11-24 18:45:12 +0000

    Separated form errors out into form_tags.py. Setup boards/tests/ folder

--- testProject/boards/tests/test_views.py
+++ testProject/boards/tests/test_views.py
@@ -1,9 +1,9 @@
 from django.urls import reverse, resolve
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .views import home, board_topics, new_topic
-from .models import Board, Topic, Post
-from .forms import NewTopicForm
+from testProject.boards.views import home, board_topics, new_topic
+from testProject.boards.models import Board, Topic, Post
+from testProject.boards.forms import NewTopicForm
 
 
 class HomeTests(TestCase):
